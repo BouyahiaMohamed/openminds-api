7f251f5b518569aa1236bc4c15e11dfc78c04375
mangeurdecookieprofessionnel <tristan.pioton@gmail.com>
2026-03-06 21:08:28 +0100

    token inch c bon

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = "OPENMINDS_SUPER_SECRET_2026";
 
 const app = express();
 app.use(cors());
@@ -20,6 +22,23 @@ db.connect((err) => {
   console.log('Connecté à la base de données MySQL');
 });
 
+const verifyToken = (req, res, next) => {
+    // On récupère le token dans l'entête "Authorization"
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // On enlève le mot "Bearer"
+
+    if (!token) return res.status(401).json({ error: "Accès refusé. Token manquant." });
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) return res.status(403).json({ error: "Session expirée ou invalide." });
+
+        // On enregistre les infos du token dans la requête pour les utiliser après
+        req.id = decoded.id;
+        req.isAdmin = decoded.isAdmin;
+        next(); // On laisse passer à la route suivante
+    });
+};
+
 app.get('/', (req, res) => {
   res.send('L\'API OpenMinds fonctionne');
 });
@@ -28,10 +47,10 @@ app.get('/', (req, res) => {
 // ROUTE 1 : INSCRIPTION (/register)
 // ==========================================
 app.post('/register', async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password , username } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json({ error: 'Email et mot de passe requis !' });
+  if (!email || !password || !username) {
+    return res.status(400).json({ error: 'Username et Email et mot de passe requis !' });
   }
 
   try {
@@ -39,8 +58,8 @@ app.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // 2. On insère dans la base de données
-    const query = 'INSERT INTO users (email, password) VALUES (?, ?)';
-    db.execute(query, [email, hashedPassword], (err, results) => {
+    const query = 'INSERT INTO users (email, password, userName, isAdmin) VALUES (?, ?, ?, 0)';
+    db.execute(query, [email, hashedPassword, username], (err, results) => {
       if (err) {
         // Si l'email existe déjà (Erreur MySQL 1062)
         if (err.code === 'ER_DUP_ENTRY') {
@@ -77,7 +96,12 @@ app.post('/login', (req, res) => {
     if (!match) {
       return res.status(401).json({ error: 'Identifiants incorrects.' });
     }
-    res.status(200).json({ message: 'Connexion réussie !', userId: user.id });
+      const token = jwt.sign(
+          { id: user.id, isAdmin: user.isAdmin },
+          SECRET_KEY,
+          { expiresIn: '24h' } // Le badge est valable 24 heures
+      );
+    res.status(200).json({ message: 'Connexion réussie !', token: token, id: user.id, isAdmin: user.isAdmin });
   });
 });
 
